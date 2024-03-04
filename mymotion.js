2b616dc8b61f69b56c33937d32667f84a7d93da5
Abdulrahman Ben Omar <abdulrahmanbenomar@gmail.com>
2024-03-04 22:55:52 +0200

    Added some javascript code, not much

---
+++ mymotion.js
@@ -0,0 +1,17 @@
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.querySelector('.close-btn');
+
+const menu = document.querySelector(".nav_links");
+const overlay = document.querySelector(".overlay");
+
+const mainThumbnail = document.querySelector(" main-thumbnail");
+const mainThumbnailLightbox = document.querySelector("..lightbox-container .main-thumbnail");
+
+const images = document.querySelectorAll(".preview img");
+
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("previous")
+
+const amount = document.querySelector(".amount");
